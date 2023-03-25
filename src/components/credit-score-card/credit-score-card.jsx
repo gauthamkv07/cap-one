3a52e4659cba1014b237a65cb2c54afb6063520c
gauthamkv07 <kvgautham010@gmail.com>
2023-03-25 17:50:34 -0400

    UI adopted for lower resolution

---
+++ src/components/credit-score-card/credit-score-card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./credit-score-card.scss";
+
+const CreditScoreCard = ()=> {
+    return <div className="credit-score-card">
+        <div className="credit-score-card-title">Credit Score</div>
+    </div>
+}
+
+export default  CreditScoreCard;
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import './homepage.scss';
 import HeaderComponent from "../components/header-component/header.component";
 import PaymentCard from "../components/payment-card/payment.card";
+import CreditScoreCard from "../components/credit-score-card/credit-score-card";
 
 const HomePage = () => {
     return <div className="homepage">
         <HeaderComponent/>
         <PaymentCard/>
+        <CreditScoreCard/>
     </div>
 }
 
