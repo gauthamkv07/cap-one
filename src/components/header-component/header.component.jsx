99c91724d62a6a21dec1ebc6da73bbbb2fdc6a8b
gauthamkv07 <kvgautham010@gmail.com>
2023-03-25 17:03:28 -0400

    folder structure changed to team style from personal

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.component.scss";
-import logo from "../asset/Capital-One-Logo.png";
+import logo from "../../asset/Capital-One-Logo.png";
 
 const HeaderComponent = () => {
     return <div className="header">
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './homepage.scss';
-import HeaderComponent from "../components/header.component";
-import PaymentCard from "../cards/payment.card";
+import HeaderComponent from "../components/header-component/header.component";
+import PaymentCard from "../components/payment-card/payment.card";
 
 const HomePage = () => {
     return <div className="homepage">
