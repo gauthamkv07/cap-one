46fabbb22deb5e0a4a027364bf491e0d6c7d0694
gauthamkv07 <kvgautham010@gmail.com>
2023-03-25 14:00:24 -0400

    base dev added

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import './homepage.scss';
+
+const HomePage = () => {
+    return <div>
+        <div>"Whatever"</div>
+    </div>
+}
+
+export default HomePage;
