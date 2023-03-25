aada725c9c5a071294ae7c9d9d19f1f3e7ada64a
gauthamkv07 <kvgautham010@gmail.com>
2023-03-25 16:28:24 -0400

    Payment card initiated

---
+++ src/cards/payment.card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./payment.card.scss";
+
+const PaymentCard = () => {
+    return <div className="payment-card">
+        <div className="card-title">
+            <div className="card-type">P L A T I N U M</div>
+            <div className="card-number"> ...1234</div>
+        </div>
+    </div>
+}
+
+export default PaymentCard;
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './homepage.scss';
 import HeaderComponent from "../components/header.component";
+import PaymentCard from "../cards/payment.card";
 
 const HomePage = () => {
-    return <div>
+    return <div className="homepage">
         <HeaderComponent/>
+        <PaymentCard/>
     </div>
 }
 
