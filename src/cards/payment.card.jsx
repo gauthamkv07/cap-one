104bb9b68ce102ebeb62321a6234a062bf420911
gauthamkv07 <kvgautham010@gmail.com>
2023-03-25 16:56:25 -0400

    balance card UI completed

--- src/cards/payment.card.jsx
+++ src/cards/payment.card.jsx
@@ -7,6 +7,23 @@ const PaymentCard = () => {
             <div className="card-type">P L A T I N U M</div>
             <div className="card-number"> ...1234</div>
         </div>
+        <div className="cur-balance-price">
+            <div className="superscript">$</div>
+            <div className="big-num">25</div>
+            <div className="superscript">53</div>
+        </div>
+        <div className="cur-balance">
+            CURRENT BALANCE
+        </div>
+        <div className="white-line"></div>
+        <div className="cur-balance-price">
+            <div className="superscript">$</div>
+            <div className="big-num">120</div>
+            <div className="superscript">47</div>
+        </div>
+        <div className="cur-balance">
+            AVAILABLE CREDIT
+        </div>
     </div>
 }
 
