8a59224993d3d480c86d087a818a17e6f5f836c0
HariVigneshG123 <86461612+HariVigneshG123@users.noreply.github.com>
2023-03-26 06:19:49 -0400

    status added for upcoming transactions

--- src/components/future-expense-card/future-expense-card.jsx
+++ src/components/future-expense-card/future-expense-card.jsx
@@ -3,9 +3,10 @@ import "./future-expense-card.scss";
 
 const FutureExpenseCard = () => {
     return <div className="future-expense-card">
-        <div className="future-score-card-title">Direct Deposit</div>
+        <div className="future-score-card-title">Upcoming transactions</div>
         <div className="flow">Inflow:  <div className="in-flow">$8765</div></div>
         <div className="flow">Outflow:<div className="out-flow">$1765</div></div>
+        <div className="flow">Status:<div className="in-flow">good</div></div>
     </div>
 }
 
