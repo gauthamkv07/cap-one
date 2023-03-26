e8bc2f553dbf5792af8670d93811a8dfe215d7b6
gauthamkv07 <kvgautham010@gmail.com>
2023-03-26 03:21:51 -0400

    added recent transactions

--- src/components/recent-transactions/recent-transactions.jsx
+++ src/components/recent-transactions/recent-transactions.jsx
@@ -2,8 +2,34 @@ import React from "react";
 import "./recent-transactions.scss";
 
 const RecentTransactions = () => {
+
+    const datas = [
+        {
+            month: "Mar",
+            day: "24",
+            receiver: "Halal Guys",
+            price: "3.12"
+        },
+        {
+            month: "Mar",
+            day: "23",
+            receiver: "Chipotle",
+            price: "8.85"
+        }
+    ]
+
     return <div className="recent-transc">
-        <div className="recent-transc-header">Recent Transactions</div>
+        <div className="recent-transc-header">
+            <div className="recent-transc-header-text">Recent Transactions</div>
+        </div>
+        {datas.map((data) => (<div className="recent-transc-row">
+            <div className="rt-data">
+                <div className="rt-month">{data.month}</div>
+                <div className="rt-day">{data.day}</div>
+                <div className="rt-receiver">{data.receiver}</div>
+            </div>
+            <div className="rt-price">${data.price}</div>
+        </div>))}
     </div>
 }
 
