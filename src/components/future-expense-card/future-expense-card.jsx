a30004d1181ecacc2158c8d56ff75c7f99421013
gauthamkv07 <kvgautham010@gmail.com>
2023-03-25 22:26:21 -0400

    added direct deposit

--- src/components/future-expense-card/future-expense-card.jsx
+++ src/components/future-expense-card/future-expense-card.jsx
@@ -3,7 +3,9 @@ import "./future-expense-card.scss";
 
 const FutureExpenseCard = () => {
     return <div className="future-expense-card">
-
+        <div className="future-score-card-title">Direct Deposit</div>
+        <div className="flow">Inflow:  <div className="in-flow">$8765</div></div>
+        <div className="flow">Outflow:<div className="out-flow">$1765</div></div>
     </div>
 }
 
