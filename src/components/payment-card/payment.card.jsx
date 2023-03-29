79ec9eb7fe9c19fb7a4c221216777736e29e3538
Achudan <achuts4@gmail.com>
2023-03-28 23:22:19 -0400

    live current balance

--- src/components/payment-card/payment.card.jsx
+++ src/components/payment-card/payment.card.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CaptialOneAPIService from "../../services/capitalOne.services";
 import "./payment.card.scss";
 
+const capService = new CaptialOneAPIService();
 const PaymentCard = () => {
+    const [data, setState] = useState(0);
+
+    useEffect(() =>{
+        const fetchData = async ()=>{
+            const res = await capService.getThisMonthAmt();
+            setState(res);
+        }
+        fetchData();
+    },[]);
     return <div className="payment-card">
         <div className="card-title">
             <div className="card-type">P L A T I N U M</div>
@@ -9,8 +20,8 @@ const PaymentCard = () => {
         </div>
         <div className="cur-balance-price">
             <div className="superscript">$</div>
-            <div className="big-num">25</div>
-            <div className="superscript">53</div>
+            <div className="big-num">{(Number.isInteger(data))?data:(data-data%1)}</div>
+            <div className="superscript">{(Number.isInteger(data))?'00':(data%1).toString().slice(0,4)}</div>
         </div>
         <div className="cur-balance">
             CURRENT BALANCE
